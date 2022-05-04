68ccdcdeb103ec8f2100d33c4c8a29a3a0f3f8e4
Nils MK <nils.meyer-kahlen@aalto.fi>
2022-05-04 18:08:42 +0300

    changed name to utility

--- startup.m
+++ startup.m
@@ -1,3 +1,6 @@
 addpath(genpath('model'))
-addpath(genpath('dependencies'))
+addpath(genpath('utility'))
 addpath(genpath('measurement_data'))
+
+% Also add the auditory modeling toolbox to the path, 
+% which can be downloaded here: https://amtoolbox.org/
